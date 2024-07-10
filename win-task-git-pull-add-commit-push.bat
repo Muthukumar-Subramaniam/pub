1080717858bb9e9e008999815fc4f8e452ae8ee8
Muthukumar Subramaniam <msmkumar.eee@gmail.com>
2024-07-10 14:06:27 +0530

    Committ from PC-Muthukumar-Subramaniam on 10-07-2024_02-06-PM

--- win-task-git-pull-add-commit-push.bat
+++ win-task-git-pull-add-commit-push.bat
@@ -1,6 +1,2 @@
-"C:\Program Files\Git\bin\bash.exe" -c "cd /c/Users/muthuks/Documents/Studies/Shell_Scripting/Scripts_by_muthu/pub/ && bash git-pull-add-commit-push.sh"
-echo Note: This was an automated execution by Windows Task Scheduler >> logs-git-pull-add-commit-push.txt 
-echo. >> logs-git-pull-add-commit-push.txt
-echo 	Task Name in Task Scheduler : Push Commits of Local Repos to GitHub and GitLab >> logs-git-pull-add-commit-push.txt
-echo. >> logs-git-pull-add-commit-push.txt
+"C:\Program Files\Git\bin\bash.exe" -c "cd /c/Users/muthuks/Documents/Studies/Shell_Scripting/Scripts_by_muthu/pub/ && bash git-pull-add-commit-push.sh && bash win-task-manager-messages.sh"
 exit
