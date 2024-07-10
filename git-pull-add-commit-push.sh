008e3b1460e5e130586ce26ae30aa57bcc604b6b
Muthukumar Subramaniam <msmkumar.eee@gmail.com>
2024-07-10 12:09:13 +0530

    Committ from PC-Muthukumar-Subramaniam on 10-07-2024_12-08-PM

---
+++ git-pull-add-commit-push.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+v_date=$(date +%d-%m-%Y_%I-%M-%p)
+
+> logs-git-pull-add-commit-push.txt
+
+{
+echo -e "\nScript execution started at $v_date . . ."
+
+echo -e "\nPublic GitHub Repository details :\n"
+
+echo -e "\n	HTTPS : https://github.com/Muthukumar-Subramaniam/pub.git\n"
+echo -e "\n	SSH   : git@github.com:Muthukumar-Subramaniam/pub.git\n"
+
+echo -e "\nPulling changes from GitHub . . .\n"
+git pull
+echo -e "\nStaging changes of local repository $(pwd) . . .\n"
+git add .
+echo -e "\nCommitting changes . . .\n"
+git commit -m "Committ from $(hostname) on $v_date"
+echo -e "\nPushing changes to GitHub . . .\n"
+git push
+v_date=$(date +%d-%m-%Y_%I-%M-%p)
+echo -e "\nScript execution completed at $v_date .\n"
+} &> logs-git-pull-add-commit-push.txt
+
+cat logs-git-pull-add-commit-push.txt
