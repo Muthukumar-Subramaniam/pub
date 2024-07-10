6149cb0637479b6c604469eb7dd0de288b385feb
Muthukumar Subramaniam <msmkumar.eee@gmail.com>
2024-07-10 14:29:58 +0530

    Committ from PC-Muthukumar-Subramaniam on 10-07-2024_02-29-PM

--- git-pull-add-commit-push.sh
+++ git-pull-add-commit-push.sh
@@ -7,11 +7,6 @@ v_date=$(date +%d-%m-%Y_%I-%M-%p)
 {
 echo -e "\nScript execution started at $v_date . . ."
 
-echo -e "\nPublic GitHub Repository details :\n"
-
-echo -e "\n	HTTPS : https://github.com/Muthukumar-Subramaniam/pub.git\n"
-echo -e "\n	SSH   : git@github.com:Muthukumar-Subramaniam/pub.git\n"
-
 echo -e "\nPulling changes from GitHub . . .\n"
 git pull
 echo -e "\nStaging changes of local repository $(pwd) . . .\n"
@@ -20,6 +15,19 @@ echo -e "\nCommitting changes . . .\n"
 git commit -m "Committ from $(hostname) on $v_date"
 echo -e "\nPushing changes to GitHub . . .\n"
 git push
+echo -e "\nPushing changes to GitLab . . .\n"
+git push origin-gitlab main
+
+echo -e "\nPublic Code Repository details :\n"
+
+echo -e "\nGitHub:"
+echo -e "\n	HTTPS : https://github.com/Muthukumar-Subramaniam/pub.git\n"
+echo -e "\n	SSH   : git@github.com:Muthukumar-Subramaniam/pub.git\n"
+
+echo -e "\nGitLab:"
+echo -e "\n	HTTPS : https://gitlab.com/muthukumar-gitlab/pub.git\n"
+echo -e "\n	SSH   : git@gitlab.com:muthukumar-gitlab/pub.git\n"
+
 v_date=$(date +%d-%m-%Y_%I-%M-%p)
 echo -e "\nScript execution completed at $v_date .\n"
 } &> logs-git-pull-add-commit-push.txt
