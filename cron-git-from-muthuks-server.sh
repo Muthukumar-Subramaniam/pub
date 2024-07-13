6c8bebbf7fc4a6011ce07020d6bc04e931a63586
Muthukumar Subramaniam <msmkumar.eee@gmail.com>
2024-07-14 01:35:07 +0530

    Commit from PC-Muthukumar-Subramaniam on 14-07-2024_01-35-AM

--- cron-git-from-muthuks-server.sh
+++ cron-git-from-muthuks-server.sh
@@ -5,7 +5,7 @@ v_next_execution_time=$(date -d "+1 hour" +%I:%M" "%p" "%Z" "%d-%m-%Y)
 ./git-pull-add-commit-push.sh
 
 {
-echo -e "Note:\n	This was an automated execution by cronjob from muthuks-server.\n"
+	echo -e "Note:\n	This was an automated execution by cronjob from $(hostname).\n"
 echo -e "	Execution takes place every 1 Hr.\n"
 echo -e "	Next Execution Starts at : ${v_next_execution_time}\n"
 } >> logs-git-pull-add-commit-push.txt
